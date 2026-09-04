d8e2817cdac9256f26a31b73832f6929b4a9b264
yash bhosale <yashbhosale@yashs-MacBook-Air.local>
2026-09-04 10:52:34 +0530

    Remove duplicate project folder

--- cooperative-gig-services-platform/core/context_processors.py
+++
@@ -1,9 +0,0 @@
-def site_context(request):
-    return {
-        "SITE_NAME": "Cooperative Gig Services",
-        "AVAILABLE_LANGUAGES": [
-            ("en", "English"),
-            ("hi", "हिन्दी"),
-            ("gu", "ગુજરાતી"),
-        ],
-    }
